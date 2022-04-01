be417d1652f9f92b1891771d9eaeef52ff48ce24
cheesecake_assassin <santasebastian@yahoo.com>
2022-04-01 15:15:17 -0400

    adding starter code from module to assist

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// API KEY: 45065de73cc83fdca8eafdd1847f8287
+// One Call API call: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=45065de73cc83fdca8eafdd1847f8287
+// Geocoding API call: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=45065de73cc83fdca8eafdd1847f8287
+
